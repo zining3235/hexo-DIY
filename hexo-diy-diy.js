f53dc4476e1098215ee719dcc423ee5fceca48d0
zining3235 <73809000+zining3235@users.noreply.github.com>
2021-02-17 00:27:03 +0800

    Update hexo-diy-diy.js

--- hexo-diy-diy.js
+++ hexo-diy-diy.js
@@ -3,7 +3,7 @@ var xiaokang = new xkTool();
 xiaokang.randomBanner(
     "https://cdn.jsdelivr.net/gh/zining3235/img/top/", // 前半部分网址
     "-bar.jpg", // 后半部分网址
-    0, // 随机数开始范围
+    001, // 随机数开始范围
     175, // 随机数结束范围
     true // 是否开启滤镜 默认不开启
 );
