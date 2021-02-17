10720cf938e6dc2350fbd8991f590830670ffcfd
zining3235 <73809000+zining3235@users.noreply.github.com>
2021-02-17 11:53:40 +0800

    Update hexo-hudie-diy.js

--- hexo-hudie-diy.js
+++ hexo-hudie-diy.js
@@ -2,7 +2,7 @@ var xiaokang = new xkTool(param1,param2);
 var xiaokang = new xkTool();
 xiaokang.changeBanner(param);
 xiaokang.randomBanner(
-    "https://cdn.jsdelivr.net/gh/zining3235/img/top/", // 前半部分网址
+    "https://github.com/zining3235/img/tree/main/top/", // 前半部分网址
     "-bar.jpg", // 后半部分网址
     001, // 随机数开始范围
     175, // 随机数结束范围
