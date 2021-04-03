995dd4d14a66052abfeaff82b4c55301a12389be
limao <5641820@qq.com>
2021-04-03 14:31:01 +0800

    删除

--- hexo-hudie-xkdiy.js
+++
@@ -1,8 +0,0 @@
-var xiaokang = new xkTool(param1,param2);
-xiaokang.randomBanner(
-    "https://cdn.jsdelivr.net/gh/zining3235/img/top/", // 前半部分网址
-    "-bar.jpg", // 后半部分网址
-    001, // 随机数开始范围
-    175, // 随机数结束范围
-    true // 是否开启滤镜 默认不开启
-);
