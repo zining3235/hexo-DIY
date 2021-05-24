0e2278dff5b4d897cded1bdd54383fbf534ad6b2
zining3235 <73809000+zining3235@users.noreply.github.com>
2021-05-24 11:43:03 +0800

    Update and rename config.js to config1.js

--- jiankong/config1.js
+++ jiankong/config1.js
@@ -2,23 +2,24 @@
 window.Config = {
 
   // 站点名
+  SiteName: '',
 
   // 站点链接
-  SiteUrl: 'https://yekei.com/',
+  SiteUrl: '/',
 
   // UptimeRobot Api 域名
   // 只需填写域名部分，默认为官网域名
   // 因官网 API 时不时的会 CROS 报错，可填自定义反代域名
   // 详见 https://github.com/yb/uptime-status/ 说明
-  ApiDomain: 'yekei.com',
+  ApiDomain: 'api.uptimerobot.com',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
     'ur1311154-39c66f13237b6c974c0dd43f',
-   //   'm784490063-7b5da437e7f1e0d67613714d',
-   //  'm784497419-de55aa09902ccb3ab22d548a',
-   //   'm784496436-71a4bf7b1e3bdf7756be131b',
+  //   'm784490063-7b5da437e7f1e0d67613714d',
+  //   'm784497419-de55aa09902ccb3ab22d548a',
+  //   'm784496436-71a4bf7b1e3bdf7756be131b',
   ],
 
   // 是否显示监测站点的链接
@@ -32,10 +33,12 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-
+  //      text: 'Homepage',
+  //      url: 'https://status.org.cn/'
     },
     {
-
+  //      text: 'GitHub',
+  //      url: 'https://github.com/yb/uptime-status'
     }
   ]
 };
